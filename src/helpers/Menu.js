84ad66af8f14eeb49f5f929a4198f57fe923644a
Uriel Scola <uriel.gomes@4all.com>
2019-06-24 10:30:24 -0300

    :hammer: Preventing scroll on iOS if menu is opened

--- src/helpers/Menu.js
+++ src/helpers/Menu.js
@@ -1,2 +1,13 @@
-export const toggleMenuBody = state =>
-  document.body ? (document.body.style.overflow = state) : null;
+export const toggleMenuBody = state => {
+  document.querySelector('html').style.overflowY = state;
+  document.body.style.overflowY = state;
+  if (state === 'hidden') {
+    document.ontouchmove = function(e) {
+      e.preventDefault();
+    };
+  } else {
+    document.ontouchmove = function(e) {
+      return true;
+    };
+  }
+};
